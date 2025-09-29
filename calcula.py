7709273c94744481a73f3e4489b19f01ffa83228
Rxz1Eaco <eacorocha@gmail.com>
2025-09-28 21:37:22 -0300

    Projeto Iniciado

---
+++ calcula.py
@@ -0,0 +1,3 @@
+nome = str(input('Qual é o seu nome : '))
+print(f'Seja bem-vindo {nome}')
+print(f'{nome}, preciso que você escolha uma dessas operações: Soma, Subtração, Divisão, Multiplicação, Exponenciação')
