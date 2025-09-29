48b14253d28fc779f10aaffafb553a29b5b7b49b
Rxz1Eaco <eacorocha@gmail.com>
2025-09-28 21:46:48 -0300

    Adicionando a def divisao

--- calcula.py
+++ calcula.py
@@ -4,19 +4,23 @@ def soma():
     soma = num1+num2
     print(f'A soma entre número {num1} e o número {num2} é {soma}')
 
-
 def subtracao():
     num1 = int(input('Digite o primeiro número: '))
     num2 = int(input('Digite o segundo número: '))
     subtracao = num1-num2
     print(f'A subtração entre número {num1} e o número {num2} é {subtracao}')
 
-
 def divisao():
     num1 = int(input('Digite o primeiro número: '))
     num2 = int(input('Digite o segundo número: '))
     divisao = num1/num2
     print(f'A divisão entre número {num1} e o número {num2} é {divisao}')
+
+def multiplicacao():
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+    multiplicacao = num1*num2
+    print(f'A multiplicação entre número {num1} e o número {num2} é {multiplicacao}')
 
 nome = str(input('Qual é o seu nome : '))
 print(f'Seja bem-vindo {nome}')
