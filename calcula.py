ec8a2650d822426859911c77fa540a6f919c96e2
Rxz1Eaco <eacorocha@gmail.com>
2025-09-28 21:39:59 -0300

    Adicionando Função Soma, Subtração + o If

--- calcula.py
+++ calcula.py
@@ -1,3 +1,29 @@
+def soma():
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+    soma = num1+num2
+    print(f'A soma entre número {num1} e o número {num2} é {soma}')
+
+
+def subtracao():
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+    subtracao = num1-num2
+    print(f'A subtração entre número {num1} e o número {num2} é {subtracao}')
+
+
 nome = str(input('Qual é o seu nome : '))
 print(f'Seja bem-vindo {nome}')
 print(f'{nome}, preciso que você escolha uma dessas operações: Soma, Subtração, Divisão, Multiplicação, Exponenciação')
+op = str(input('Qual operação você deseja: '))
+
+
+if op == 'Soma':
+    soma()
+
+elif op == 'Subtração':
+    subtracao()
+
+
+else:
+    print('Operação Incorreta, tente novamente')
