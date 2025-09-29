6013bf2c1df38270ac7c9b9fd7c2547f7c0c876c
Rxz1Eaco <eacorocha@gmail.com>
2025-09-28 21:43:02 -0300

    Adicionando def divisao

--- calcula.py
+++ calcula.py
@@ -12,6 +12,12 @@ def subtracao():
     print(f'A subtração entre número {num1} e o número {num2} é {subtracao}')
 
 
+def divisao():
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+    divisao = num1/num2
+    print(f'A divisão entre número {num1} e o número {num2} é {divisao}')
+
 nome = str(input('Qual é o seu nome : '))
 print(f'Seja bem-vindo {nome}')
 print(f'{nome}, preciso que você escolha uma dessas operações: Soma, Subtração, Divisão, Multiplicação, Exponenciação')
@@ -23,6 +29,9 @@ if op == 'Soma':
 
 elif op == 'Subtração':
     subtracao()
+
+elif op == 'Divisão':
+    divisao()
 
 
 else:
